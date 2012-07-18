2c55cafe11de58500daf3daa0cde7e7048cf72ef
chenzhao <remaintears@gmail.com>
2012-07-18 11:30:52 +0800

    expand

--- application/views/admin/expand/new_expand.php
+++ application/views/admin/expand/new_expand.php
@@ -6,7 +6,7 @@
 		<base href="<?php echo base_url(); ?>"/>
 		<base src="<?php echo base_url(); ?>"/>
 		<?php $this -> load -> view("admin/global_source"); ?>
-		<?php $this -> load -> view("admin/expand/global_source"); ?>
+		<?php $this -> load -> view("admin/expand/expand_common"); ?>
 	</head>
 	<body>
 		<?php $this -> load -> view('admin/header'); ?>

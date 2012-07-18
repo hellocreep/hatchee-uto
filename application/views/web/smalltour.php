89f056ea848f57763ddfbb9d2dc639160abab15c
welkang <welkang@qq.com>
2012-07-18 12:02:46 +0800

    主题旅行的侧边栏

--- application/views/web/smalltour.php
+++ application/views/web/smalltour.php
@@ -25,7 +25,39 @@
 				<div class="breadcrumb">
 					您的位置：<a href="<?php echo base_url();?>">首页</a> > <a href="<?php echo base_url();?>themetour">主题旅行</a> > <?php echo $tour[0] -> name;?>
 				</div>
-				<div class="aside"></div>
+				<div class="aside side-thme">
+					<dl>
+						<dt>目的地</dt>
+						<dd>四姑娘山</dd>
+						<dd>四姑娘山</dd>
+						<dd>贡嘎山</dd>
+						<dd>丹巴</dd>
+						<dd>九寨</dd>
+						<dd>青城山</dd>
+						<dd>峨嵋乐山</dd>
+						<dd>稻城亚丁</dd>
+						<dd>西昌</dd>
+						<dd>新都桥</dd>		
+					</dl>
+					<dl>
+						<dt>旅行主题</dt>
+						<dd>休闲度假</dd>
+						<dd>摄影旅行</dd>
+						<dd>美食之旅</dd>
+						<dd>徒步之旅</dd>
+						<dd>心灵之旅</dd>
+						<dd>公益之旅</dd>
+					</dl>
+					<dl>
+						<dt>节假日出行</dt>
+						<dd>暑假</dd>
+						<dd>中秋</dd>
+						<dd>国庆</dd>
+						<dd>元旦</dd>
+						<dd>寒假</dd>
+						<dd>春节</dd>
+					</dl>
+				</div>
 				<div class="article">
 					<input type="hidden" value="<?php echo $tour[0]->Id;?>" id="tour_id">
 					<input type="hidden" value="<?php echo $tour[0]->days;?>" id="tour_day">

896e2f7e790877b5236093fab26d41affd24e387
hellocreep <hellocreep@gmail.com>
2012-07-19 22:57:20 +0800

    更新了一些奇怪的东西

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -1,7 +1,7 @@
 <div class="navbar">
 	<div class="navbar-inner">
 		<div class="container">
-			<span class="brand">UTO后台管理系统</span>
+			<span class="brand animated hinge"><span id="kidding">UTO</span>后台管理系统<sup>Beta 1.0</sup></span>
 			
 			<ul class="nav pull-right">
 				<li><a href="manage/loginout">退出</a></li>
--- application/views/admin/mainbody.php
+++ application/views/admin/mainbody.php
@@ -9,6 +9,15 @@
 		</div>
 		<div class="well" id="welcome-message">
 			<h2>欢迎使用UTO后台管理系统</h2>
+			<ul class="message-list">
+				<li>这是<a href="http://utotrip.com" target="_blank">友途</a>网站后台管理系统的<span class="label label-success">第一个</span>版本</li>
+				<li>时间不多只完成了线路，游记，订单等基本需求，更多功能将在以后添加</li>
+				<li>登录时请务必使用自己的帐号，尽量不要使用<span class="label label-important">utoadmin</span>帐号登录，否则数据被删除将无法找回</li>
+				<li>在填写后台数据时，你会看到<code>*</code>或<code>+</code>等奇怪的符号，这是我们所使用的<a href="http://wowubuntu.com/markdown/">Markdown</a>标记语言，希望你能花5分钟学习一下</li>
+				<li>这不是一个完美的系统，它也存在Bug，请在操作时尽量温柔对待，如遇问题请即时向我们反馈</li>
+				<li>建议你使用Firefox或者Chrome等现代浏览器，如在一些奇怪的浏览器下出现问题，那就自求多福吧</li>
+				<li>如果你对现在的后台有任何<span class="label label-warning">新的需求</span>，请仔细斟酌之后向我们提出，我们会尽力完成</li>
+			</ul>
 		</div>
 		<p id="tool-bar"></p>
 		<table class="table table-bordered table-striped">

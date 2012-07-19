24f418dd3ffe7b270cc50a8aa45e1f8d837f5f0f
liwen <hellocreep@gmail.com>
2012-07-19 16:46:22 +0800

    主页添加slide

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -11,22 +11,58 @@
 		<script type="text/javascript" src="assets/My97DatePicker/WdatePicker.js"></script>
 		<script type="text/javascript" src="assets/My97DatePicker/calendar.js"></script>
 		<?php $this -> load -> view("web/global_source");?>
+		
+		<script type="text/javascript" src="assets/scripts/web/bootstrap-carousel.js"></script>
 	</head>
 	<body id="n-index">
 		<div class="wrapper">
 			<?php $this->load->view('web/header'); ?>
-			
+				
+			<script type="text/javascript">
+				(function(){
+					$(function(){
+						$('.carousel').carousel({
+							interval: 2000
+						});
+					})
+				})(jQuery)
+			</script>
 			<div class="content clearfix">
 				<div class="homelt">
-					<ul class="slide">
-						<li>
-							<img src="assets/images/banner-big.jpg">
-						</li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-					</ul>
+					<div class="slide carousel" id="myCarousel">
+						<div class="carousel-inner">
+							<div class="active item">
+								<img src="assets/images/banner-big.jpg">
+								<div class="carousel-caption">
+									<h4>test0</h4>
+									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+								</div>
+							</div>
+							<div class="item">
+								<img src="assets/images/banner-big.jpg">
+								<div class="carousel-caption">
+									<h4>test1</h4>
+									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+								</div>
+							</div>
+							<div class="item">
+								<img src="assets/images/banner-big.jpg">
+								<div class="carousel-caption">
+									<h4> test2</h4>
+									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+								</div>
+							</div>
+						</div>
+						<!-- <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+						<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a> -->
+
+						<ul class="slide-num clearfix">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+						</ul>
+
+					</div>
 
 					<div class="col col-tour">
 						<h2>友途活动</h2>
